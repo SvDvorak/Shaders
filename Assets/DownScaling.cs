fa66cc628e4ce433015aa64af15118839bb50018
Andreas Wilcox <unknownbum@gmail.com>
2015-08-03 11:49:53 +0200

    Initial add with down scaling shader

---
+++ Assets/DownScaling.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent (typeof (Camera))]
+public class DownScaling : MonoBehaviour
+{
+    public Shader shader;
+
+    [Range(1, 15)]
+    public int Amount;
+
+    private Material _material;
+
+    public void Start()
+    {
+        // Disable if we don't support image effects
+        if (!SystemInfo.supportsImageEffects)
+        {
+            enabled = false;
+            return;
+        }
+
+        // Disable the image effect if the shader can't
+        // run on the users graphics card
+        if (!shader || !shader.isSupported)
+            enabled = false;
+    }
+
+    [ImageEffectOpaque]
+    public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        material.SetInt("_DownScaling", Amount);
+        Graphics.Blit(source, destination, _material);
+    }
+
+    private Material material
+    {
+        get
+        {
+            if (_material == null)
+            {
+                _material = new Material(shader);
+                _material.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return _material;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_material)
+        {
+            DestroyImmediate(_material);
+        }
+    }
+}
---
+++ Assets/DownScaling.shader
@@ -0,0 +1,32 @@
+﻿Shader "Custom/DownScaling"
+{
+	Properties
+	{
+		_MainTex("Base (RGB)", 2D) = "" {}
+		_LowResAmount("DownScaling", int) = 10
+	}
+	Subshader
+	{
+		Pass{
+		ZTest Always Cull Off ZWrite Off
+		CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+
+			uniform sampler2D _MainTex;
+			uniform int _DownScaling;
+
+			fixed4 frag(v2f_img i) : SV_Target
+			{
+				half2 newUV = i.uv * _ScreenParams.xy;
+				float u = (floor(newUV.x / _DownScaling)*_DownScaling) / _ScreenParams.x;
+				float v = (floor(newUV.y / _DownScaling)*_DownScaling) / _ScreenParams.y;
+	
+				return tex2D(_MainTex, float2(u,v));
+			}
+		ENDCG
+		}
+	}
+	Fallback off
+}
